211774fdf4d5d14dfe2def451a6f011e7ac46253
StefanaBotezatu <stefana.botezatu@bertelsmann.de>
2025-09-16 11:48:52 +0000

    updates

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Activities from './components/Activities';
 import Leaderboard from './components/Leaderboard';
 import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
-
 import logo from '../public/octofitapp-small.png';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand d-flex align-items-center" to="/">
-            <img src={logo} alt="OctoFit Logo" className="App-logo" />
+            <img src={logo} alt="OctoFit Logo" className="octofit-logo" />
             OctoFit Tracker
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
